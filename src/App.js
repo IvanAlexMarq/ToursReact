50d618d9d8488ce69e42bf7d588e1f3e6abf3e98
Ivan <ivanalexmarquez@gmail.com>
2021-09-13 15:23:47 -0400

    eliminando un tour+final

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   const [loading,setLoading]=useState(true);
   const [tours,setTours]=useState([]);
 
+  //funcion para eliminar un tour
+  const removeTour=id=>{
+    const newTours=tours.filter(tour=>tour.id!==id);
+    setTours(newTours);
+  }
+
   //funcion para llamar a la api
   const fetchTours=async()=>{
     setLoading(true);
@@ -34,9 +40,19 @@ function App() {
       </main>
     )
   }
+  if(tours.length===0){
+    return(
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button onClick={fetchTours} className="btn">refresh</button>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
-      <Tours tours={tours}/>
+      <Tours tours={tours} removeTour={removeTour}/>
     </main>
   );
 }

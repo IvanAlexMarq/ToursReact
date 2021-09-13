de10f664cda4be3f9658655d0e4ae47ffe34abef
Ivan <ivanalexmarquez@gmail.com>
2021-09-13 14:32:06 -0400

    llamando a la api y cargando al state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url='https://course-api.com/react-tours-project';
 
 function App() {
+  const [loading,setLoading]=useState(true);
+  const [tours,setTours]=useState([]);
+
+  //funcion para llamar a la api
+  const fetchTours=async()=>{
+    setLoading(true);
+    //para cuando exista error llamando a la api
+    try {
+      const response=await fetch(url);
+      const tours=await response.json();
+      setTours(tours);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+    
+  }
+  useEffect(()=>{
+    fetchTours();
+  },[]);
+
+  if(loading){
+    return(
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
   return (
-    <h1>Tours Proyect Setup</h1>
+    <main>
+      <Tours/>
+    </main>
   );
 }
 
---
+++ src/Tours.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tours = () => {
+    return (
+        <div>
+            <h1>Tours Component</h1>
+        </div>
+
+    );
+}
+ 
+export default Tours;
